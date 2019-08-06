c32f19bbf9458e8bf24773cf53784ff160b3f7b4
bit <bit@DESKTOP-1LOO2I6>
2019-08-06 16:19:50 +0900

    1번 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -1,6 +1,24 @@
 package practice01.prob1;
 
+import java.util.Scanner;
+
 public class Prob1 {
 	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요 : ");
+		int number = scanner.nextInt();
+		
+		System.out.println(number);
+		
+		if(number%3 == 0) {
+			System.out.print("3의 배수입니다.");
+		}
+		
+		
+		else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+	
 	}
 }
