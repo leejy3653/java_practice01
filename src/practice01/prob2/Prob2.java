8bd7a846cbe107670c90ebd09cd689616789d55c
bit <bit@DESKTOP-1LOO2I6>
2019-08-06 21:33:42 +0900

    완료

--- src/practice01/prob2/Prob2.java
+++ src/practice01/prob2/Prob2.java
@@ -1,6 +1,15 @@
 package practice01.prob2;
 
 public class Prob2 {
-	public static void main (String args[]) {
-	}
+	public static void main(String args[]) {
+		int val = 0;
+        for(int i =0; i<10; i++) {  
+            for(int j=1; j<10; j++) {
+            	val = i + j;
+                System.out.print(val + " ");  
+            }
+            System.out.println(""); 
+
+        }
+    }
 }
--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,28 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
-	public static void main (String args[]) {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int val = 0;
+		  System.out.print("숫자를 입력하세요");
+		  
+		int number = scanner.nextInt();
+
+		  System.out.println(number);
+		  
+		  if(number%2==0) {
+			  for(int i=0; i<=number; i++) {
+				val=(2+number)*((number)/2)/2; //짝수의 합 공식
+			  }
+		  } else {
+			  val=((number+1)/2)*((number+1)/2); //홀수의 합 공식
+				  }
+		  System.out.print("결과값 : "+val);
+			  
+		  }
 	}
-}
+	
+
+		
