12b6c0ad148309c1db100d2f3c00aff5571cce2b
leejy <leejy@DESKTOP-G03DF88>
2019-08-11 18:48:10 +0900

    완료

--- src/practice01/prob4/Prob4.java
+++ src/practice01/prob4/Prob4.java
@@ -1,6 +1,24 @@
 package practice01.prob4;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("단어를 입력해주세요");
+		String word = sc.nextLine();
+
+		String[] array_word = new String[word.length()]; // 스트링을 담을 배열
+		String result = ""; 
+
+		for(int i=0;i<array_word.length;i++){ //스트링을 한글자씩 끊어 배열에 저장
+			array_word[i]=Character.toString(word.charAt(i));
+		}
+				
+		for(int i=0;i<array_word.length;i++) {
+			result=array_word[i]; //
+		}
+						
+		System.out.println(result); //글자  출력
 	}
 }
--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,23 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		for(int i=1 ; i<100; i++) {
+			String[] s = Integer.toString(i).split("");
+			int n=0;
+			for(int j=0 ; j<s.length ; j++) {
+				if(Integer.parseInt(s[j])%3==0 && Integer.parseInt(s[j])!=0) {
+					n++;
+				}
+			}
+			
+			if(n==2) {
+				System.out.println(i+" 짝짝");
+				
+			}else if(n==1) {
+				System.out.println(i+" 짝");
+				
+			}
+		}
+		
 	}
 }
